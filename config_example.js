ea0d10d3245996ef8a47ec68a587c1703152b86b
Marco Andronaco <andronacomarco@gmail.com>
2021-05-16 01:06:38 +0200

    initial commit

---
+++ config_example.js
@@ -0,0 +1,29 @@
+let theme = "based";
+
+let categories = [
+    {
+        name: "Useful links",
+        links: [
+            "https://discord.com/app Discord",
+            "https://youtube.com YouTube",
+            "https://twitch.tv Twitch",
+            "https://facebook.com Facebook",
+            "https://instagram.com Instagram",
+            "https://web.whatsapp.com WhatsApp",
+            "https://amazon.it Amazon",
+            "https://outlook.com Outlook",
+            "https://twitter.com Twitter"
+        ]
+    },
+    {
+        name: "Your own links",
+        links: [
+            "# Link 1",
+            "# Link 2",
+            "# Link 3",
+            "# Link 4",
+            "# Link 5",
+            "# Link 6"
+        ]
+    }
+];
